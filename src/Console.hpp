2061a5b4a3094ed2dcc60813044474d8f47eb4cd
Nikita <shockck84@gmail.com>
2018-08-06 16:24:36 +0500

    Clean up and comment code

--- src/Console.hpp
+++ src/Console.hpp
@@ -6,6 +6,7 @@
 using namespace GarrysMod::Lua;
 using std::string;
 
+// Provides access for Garry's Mod developer console logging.
 class Console {
 public:
 	Console(ILuaBase *lua) : lua(lua) {};
@@ -15,6 +16,12 @@ public:
 
 	// Prints "[GPython] " + message to the console.
 	void log(string message);
+
+	// Prints "[GPython] ERROR: " + message to the console.
+	void error(string message);
+
+	// Prints "[GPython] WARN: " + message to the console.
+	void warn(string message);
 
 private:
 	ILuaBase *lua;
--- src/addon_launcher.hpp
+++ src/addon_launcher.hpp
@@ -7,4 +7,5 @@
 
 #include "Console.hpp"
 
+// Runs __init__.py scripts of all Python addons from PYTHON_ADDONS_PATH directory.
 void launchAddons(Console& cons);

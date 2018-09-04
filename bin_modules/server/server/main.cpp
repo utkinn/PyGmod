9d07e5109cb30045f9a6528144a87a5a2cdf2ba4
Nikita <shockck84@gmail.com>
2018-09-04 18:36:07 +0500

    Add "server" module

---
+++ bin_modules/server/server/main.cpp
@@ -0,0 +1,15 @@
+#define GMMODULE
+#define DLL_IMPORT extern "C" __declspec(dllimport)
+
+#include "Interface.h"
+
+DLL_IMPORT int gpython_run(lua_State *state, bool client);
+DLL_IMPORT int gpython_finalize(lua_State* state);
+
+GMOD_MODULE_OPEN() {
+    return gpython_run(state, false);
+}
+
+GMOD_MODULE_CLOSE() {
+    return gpython_finalize(state);
+}

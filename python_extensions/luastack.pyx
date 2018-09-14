82c498fb5c95e39467bdca32a189b13cd0ae1451
Nikita <shockck84@gmail.com>
2018-09-14 15:44:30 +0500

    luastack functions are no-ops when used outside of Garry's Mod

--- python_extensions/luastack.pyx
+++ python_extensions/luastack.pyx
@@ -112,13 +112,33 @@ type_names = [
 # Being set in setup().
 cdef ILuaBase* lua = NULL
 
+# Guard variable for safe documentation generation.
+# Stack functions are no-ops if this equals False.
+IN_GMOD = False
+
 
 cdef public setup(ILuaBase* base):
     """This function is called in ``giveILuaBasePtrToLuastack()`` in ``main.cpp`` of the C++ module."""
     global lua
+    global IN_GMOD
     lua = base
+    IN_GMOD = True
 
 
+def _stack(func):
+    """Decorator for stack manipulation functions.
+
+    Makes the decorated function to be called only if :const:`IN_GMOD` is `True`.
+    """
+
+    def decorated(*args, **kwargs):
+        if IN_GMOD:
+            func(*args, **kwargs)
+
+    return decorated
+
+
+@_stack
 def top():
     """Returns the index of the top element in the stack.
 
@@ -128,6 +148,7 @@ def top():
     return lua.Top()
 
 
+@_stack
 def equal(int a, int b):
     """
     Returns ``True`` if the two values in acceptable indices ``a`` and ``b`` are equal,
@@ -137,6 +158,7 @@ def equal(int a, int b):
     return <bool> lua.Equal(a, b)
 
 
+@_stack
 def raw_equal(int a, int b):
     """
     Returns ``True`` if the two values in acceptable indices
@@ -146,11 +168,13 @@ def raw_equal(int a, int b):
     return <bool> lua.RawEqual(a, b)
 
 
+@_stack
 def insert(int destination_index):
     """Moves the top element into the given valid index, shifting up the elements above this index to open space."""
     lua.Insert(destination_index)
 
 
+@_stack
 def throw_error(const char* message):
     """Raises an error with the given :class:`bytes` message.
 
@@ -160,49 +184,59 @@ def throw_error(const char* message):
     lua.ThrowError(message)
 
 
+@_stack
 def create_table():
     """Creates a new empty table and pushes it onto the stack."""
     lua.CreateTable()
 
 
+@_stack
 def push(int stack_pos):
     lua.Push(stack_pos)
 
 
+@_stack
 def push_special(int type):
     lua.PushSpecial(type)
 
 
+@_stack
 def push_nil():
     """Pushes a ``nil`` value onto the stack."""
     lua.PushNil()
 
 
+@_stack
 def push_number(double num):
     """Pushes a number with value ``num`` onto the stack."""
     lua.PushNumber(num)
 
 
+@_stack
 def push_string(const char* val):
     """Pushes a ``bytes`` onto the stack."""
     lua.PushString(val)
 
 
+@_stack
 def push_bool(bool val):
     """Pushes a boolean value ``val`` onto the stack."""
     lua.PushBool(val)
 
 
+@_stack
 def pop(int amt=1):
     """Pops ``amt`` elements from the stack."""
     lua.Pop(amt)
 
 
+@_stack
 def clear():
     """Clears the stack."""
     pop(top())
 
 
+@_stack
 def get_table(int stack_pos):
     """
     Pushes onto the stack the value ``t[k]``, where ``t`` is the value at the given valid index ``stack_pos``
@@ -213,11 +247,13 @@ def get_table(int stack_pos):
     lua.GetTable(stack_pos)
 
 
+@_stack
 def get_field(int stack_pos, const char* name):
     """Pushes onto the stack the value ``t[name]``, where ``t`` is the value at the given valid index ``stack_pos``."""
     lua.GetField(stack_pos, name)
 
 
+@_stack
 def set_table(int stack_pos):
     """
     Does the equivalent to ``t[k] = v``,
@@ -229,6 +265,7 @@ def set_table(int stack_pos):
     lua.SetTable(stack_pos)
 
 
+@_stack
 def set_field(int stack_pos, const char* name):
     """
     Does the equivalent to ``t[k] = v``,
@@ -240,6 +277,7 @@ def set_field(int stack_pos, const char* name):
     lua.SetField(stack_pos, name)
 
 
+@_stack
 def call(int args, int results):
     """Calls a function.
 
@@ -277,6 +315,7 @@ def call(int args, int results):
     lua.Call(args, results)
 
 
+@_stack
 def get_string(int stack_pos=-1):
     """Returns the ``bytes`` value of a string item at ``stack_pos`` index of the stack.
 
@@ -285,6 +324,7 @@ def get_string(int stack_pos=-1):
     return lua.GetString(stack_pos, <unsigned int*> 0)
 
 
+@_stack
 def get_number(int stack_pos=-1):
     """Returns the ``float`` value of a number item at ``stack_pos`` index of the stack.
 
@@ -293,6 +333,7 @@ def get_number(int stack_pos=-1):
     return lua.GetNumber(stack_pos)
 
 
+@_stack
 def get_bool(int stack_pos=-1):
     """Returns the boolean value of a boolean item at ``stack_pos`` index of the stack.
 
@@ -301,28 +342,34 @@ def get_bool(int stack_pos=-1):
     return lua.GetBool(stack_pos)
 
 
+@_stack
 def create_ref():
     """Saves the value at the top of the stack to a reference, pops it and returns the reference."""
     return lua.ReferenceCreate()
 
 
+@_stack
 def free_ref(int ref):
     """Frees the reference."""
     lua.ReferenceFree(ref)
 
 
+@_stack
 def push_ref(int ref):
     """Pushes the value saved in the reference."""
     lua.ReferencePush(ref)
 
 
+@_stack
 def get_type(int stack_pos):
     return lua.GetType(stack_pos)
 
 
+@_stack
 def get_type_name(ValueType type):
     return lua.GetTypeName(type)
 
 
+@_stack
 def is_type(int stack_pos, ValueType type):
     return lua.IsType(stack_pos, type)

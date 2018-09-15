3b026b4c5ce7a92b638da574bf7c5b7622059e64
Nikita <shockck84@gmail.com>
2018-09-15 19:11:07 +0500

    Rename py.exec to py.Exec

--- src/lua2py_interop.cpp
+++ src/lua2py_interop.cpp
@@ -4,7 +4,7 @@
 #define LUA_FUNC(name) int name(lua_State *state)
 
 // Eexcutes a string of Python code.
-LUA_FUNC(py_exec) {
+LUA_FUNC(py_Exec) {
     const char *code = LUA->CheckString();
 
     PyRun_SimpleString(code);
@@ -16,8 +16,8 @@ void extendLua(ILuaBase *lua) {
     lua->PushSpecial(SPECIAL_GLOB);
     lua->CreateTable();  // To be "py" table
 
-    lua->PushCFunction(py_exec);
-    lua->SetField(-2, "exec");
+    lua->PushCFunction(py_Exec);
+    lua->SetField(-2, "Exec");
 
     // Adding "py" table to the global namespace
     lua->SetField(-2, "py");

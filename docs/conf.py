1ab798c6f806dd946512be73d4b945b144f7048b
Nikita <shockck84@gmail.com>
2018-09-16 12:51:58 +0500

    Bring back Corlab theme

--- docs/conf.py
+++ docs/conf.py
@@ -80,10 +80,10 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# import corlab_theme
-# html_theme      = 'corlab_theme'
-# html_theme_path = [ corlab_theme.get_theme_dir() ]
-html_theme = 'bizstyle'
+import corlab_theme
+html_theme      = 'corlab_theme'
+html_theme_path = [ corlab_theme.get_theme_dir() ]
+# html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

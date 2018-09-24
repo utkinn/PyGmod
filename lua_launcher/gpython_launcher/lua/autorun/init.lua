add90fdf7a45017074ccd9cf8962d0ecd4553fc4
Nikita <shockck84@gmail.com>
2018-09-24 08:20:33 +0500

    Clarify error message in lua launcher

--- lua_launcher/gpython_launcher/lua/autorun/init.lua
+++ lua_launcher/gpython_launcher/lua/autorun/init.lua
@@ -1,4 +1,4 @@
-local success = pcall(require, 'gpython')
+local success, errorMessage = pcall(require, 'gpython')
 if not success then
-    error('[GPython] ERROR: GPython binary module is not installed or installed incorrectly.')
+    error('[GPython] ERROR: GPython binary module is not installed or installed incorrectly. ('..errorMessage..')')
 end

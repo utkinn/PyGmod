41c6bbbb900e4facb1b32b2724a63677d6dabde7
Nikita <shockck84@gmail.com>
2018-10-10 22:23:47 +0500

    Move all luastack funcs to LuaStack class
    This helps the testability.

--- python_extensions/gmod/lua.py
+++ python_extensions/gmod/lua.py
@@ -5,10 +5,11 @@ and calling functions.
 
 from abc import ABC, abstractmethod
 
-from luastack import *
+from luastack import LuaStack, Special
 
+__all__ = ['G', 'exec', 'eval', 'table', 'LuaObjectWrapper']
 
-__all__ = ['G', 'lua_exec', 'lua_eval', 'table', 'LuaObjectWrapper']
+ls = LuaStack()
 
 
 class Reference:
@@ -16,28 +17,28 @@ class Reference:
         self._ref = int(ref)
 
     def __enter__(self):
-        push_ref(self._ref)
+        ls.push_ref(self._ref)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        pop(1)
+        ls.pop(1)
 
 
 def push_key_or_value(key_or_value):
     """Pushes a Python value of a primitive type or a :class:`LuaObject`."""
     if key_or_value is None:
-        push_nil()
+        ls.push_nil()
     if isinstance(key_or_value, int) or isinstance(key_or_value, float):
-        push_number(key_or_value)
+        ls.push_number(key_or_value)
     elif isinstance(key_or_value, LuaObject):
-        push_ref(key_or_value._ref)
+        ls.push_ref(key_or_value._ref)
     elif isinstance(key_or_value, str):
-        push_string(key_or_value.encode())
+        ls.push_string(key_or_value.encode())
     elif isinstance(key_or_value, bytes):
-        push_string(key_or_value)
+        ls.push_string(key_or_value)
     elif isinstance(key_or_value, bool):
-        push_bool(key_or_value)
+        ls.push_bool(key_or_value)
     elif isinstance(key_or_value, LuaObjectWrapper):
-        push_ref(key_or_value.lua_obj._ref)
+        ls.push_ref(key_or_value.lua_obj._ref)
     else:
         raise TypeError(f'unsupported key/value type: {type(key_or_value)}')
 
@@ -45,59 +46,59 @@ def push_key_or_value(key_or_value):
 class LuaObject:
     def __init__(self):
         """Creates a :class:`LuaObject` which points to the topmost stack value and pops it."""
-        self._ref = create_ref()
+        self._ref = ls.create_ref()
         self._context = Reference(self._ref)
 
     def __del__(self):
-        free_ref(self._ref)
+        ls.free_ref(self._ref)
 
     @property
     def type(self):
         """Returns the :class:`luastack.ValueType` of the held value."""
         with self._context:
-            return get_type(-1)
+            return ls.get_type(-1)
 
     @property
     def type_name(self):
         """Returns the :class:`str` type representation of the held value."""
         with self._context:
-            return get_type_name(get_type(-1))
+            return ls.get_type_name(ls.get_type(-1))
 
     def __str__(self):
         with self._context:
-            return get_string(-1)
+            return ls.get_string(-1)
 
     def __int__(self):
         with self._context:
-            return int(get_number(-1))
+            return int(ls.get_number(-1))
 
     def __float__(self):
         with self._context:
-            return float(get_number(-1))
+            return float(ls.get_number(-1))
 
     def __bool__(self):
         with self._context:
-            return get_bool(-1)
+            return ls.get_bool(-1)
 
     def __setitem__(self, key, value):
         with self._context:
             push_key_or_value(key)
             push_key_or_value(value)
-            set_table(-3)
+            ls.set_table(-3)
 
     def __getitem__(self, key):
         with self._context:
             push_key_or_value(key)
-            get_table(-2)
+            ls.get_table(-2)
             return LuaObject()
 
     def __call__(self, *args):
-        push_ref(self._ref)
+        ls.push_ref(self._ref)
         for val in args:
             push_key_or_value(val)
-        call(len(args), -1)
+        ls.call(len(args), -1)
         returns = []
-        while top():
+        while ls.top():
             returns.insert(0, LuaObject())
         if len(returns) == 1:
             return returns[0]
@@ -106,40 +107,40 @@ class LuaObject:
 
 
 # Lua global table
-if IN_GMOD:
-    push_special(Special.GLOBAL)
+if ls.IN_GMOD:
+    ls.push_special(Special.GLOBAL)
     G = LuaObject()
 else:
     G = None
 
 
-def lua_exec(code):
+def exec(code):
     """Executes the given Lua code block. Returns nothing.
 
     ::
 
         code = 'MsgN("test")'
-        lua_exec(code)  # 'test' will be printed to the console
+        lua.exec(code)  # 'test' will be printed to the console
     """
-    push_special(Special.GLOBAL)
+    ls.push_special(Special.GLOBAL)
 
-    get_field(-1, 'RunString')
-    push_string(code.encode())  # Arg 1: code
-    push_string('GPython lua_exec')  # Arg 2: identifier
-    push_bool(True)  # Arg 3: throw error if error occurred during code exec
+    ls.get_field(-1, 'RunString')
+    ls.push_string(code.encode())  # Arg 1: code
+    ls.push_string('GPython lua.exec')  # Arg 2: identifier
+    ls.push_bool(True)  # Arg 3: throw error if error occurred during code exec
 
-    call(3, 0) # Call RunString and pop it from the stack
+    ls.call(3, 0)  # Call RunString and pop it from the stack
 
-    pop(1)  # GLOBAL
+    ls.pop(1)  # GLOBAL
 
 
-def lua_eval(expr):
+def eval(expr):
     """Evaluates a single Lua expression. Returns :class:`LuaObject` with evaluation result.
 
     ::
 
         expr = 'game.SinglePlayer()'  # Returns "true" if the current session is a single player game
-        single_player_luaobj = lua_eval(expr)
+        single_player_luaobj = lua.eval(expr)
         # Remember that we need to convert the evaluation result to bool explicitly
         single_player = bool(single_player_luaobj)
 
@@ -147,47 +148,47 @@ def lua_eval(expr):
         if single_player:
             ...
     """
-    push_special(Special.GLOBAL)
+    ls.push_special(Special.GLOBAL)
 
-    get_field(-1, 'RunString')
-    push_string(f'_gpy_temp = {expr}'.encode())  # Assign expression to temporary var _gpy_temp
-    push_string('GPython lua_eval')
-    push_bool(True)
+    ls.get_field(-1, 'RunString')
+    ls.push_string(f'_gpy_temp = {expr}'.encode())  # Assign expression to temporary var _gpy_temp
+    ls.push_string('GPython lua.eval')
+    ls.push_bool(True)
 
-    call(3, 0)
+    ls.call(3, 0)
 
     # Grabbing the result from _gpy_temp
-    get_field(-1, '_gpy_temp')
+    ls.get_field(-1, '_gpy_temp')
     obj = LuaObject()
 
     # Cleaning up: setting _gpy_temp to nil
-    push_nil()
-    set_field(-2, '_gpy_temp')
+    ls.push_nil()
+    ls.set_field(-2, '_gpy_temp')
 
-    pop(1)  # GLOBAL
+    ls.pop(1)  # GLOBAL
     return obj
 
 
 def table(iterable):
     """Creates and returns a :class:`LuaObject` of a new Lua table from ``iterable``."""
-    create_table()
-    push_special(Special.GLOBAL)
-    get_field(-1, 'table')
+    ls.create_table()
+    ls.push_special(Special.GLOBAL)
+    ls.get_field(-1, 'table')
     for v in iterable:
-        get_field(-1, 'insert')
-        push(1)  # Repush that new table
+        ls.get_field(-1, 'insert')
+        ls.push(1)  # Pushing that new table again
         try:
             push_key_or_value(v)
-        except TypeError:  # In case of unpushable value
-            clear()
-            raise  # Reraising TypeError
-        call(2, 0)
-    pop(2)  # Pop the 'table' namespace and the global table
+        except TypeError:  # In case of a value that can't be pushed
+            ls.clear()
+            raise  # Raising TypeError again
+        ls.call(2, 0)
+    ls.pop(2)  # Pop the 'table' namespace and the global table
 
     return LuaObject()  # The new table is grabbed and popped by the LuaObject's constructor
 
 
-class LuaObjectWrapper:
+class LuaObjectWrapper(ABC):
     """Abstract class for Lua class wrappers, such as :class:`gmod.entity.Entity`.
 
     Subclasses must implement a ``lua_obj`` property that should return the wrapped :class:`LuaObject`.
--- python_extensions/luastack.pyx
+++ python_extensions/luastack.pyx
@@ -128,248 +128,249 @@ cdef public setup(ILuaBase* base):
 def _stack(func):
     """Decorator for stack manipulation functions.
 
-    Makes the decorated function to be called only if :const:`IN_GMOD` is ``True``.
+    Makes the decorated function to be a static method and act only if :const:`IN_GMOD` is ``True``.
     """
 
     def decorated(*args, **kwargs):
         if IN_GMOD:
             return func(*args, **kwargs)
 
-    return decorated
+    return staticmethod(decorated)
 
 
-@_stack
-def top():
-    """Returns the index of the top element in the stack.
+class LuaStack:
+    @_stack
+    def top():
+        """Returns the index of the top element in the stack.
 
-    Because indices start at 1,
-    this result is equal to the number of elements in the stack (and so 0 means an empty stack).
-    """
-    return lua.Top()
+        Because indices start at 1,
+        this result is equal to the number of elements in the stack (and so 0 means an empty stack).
+        """
+        return lua.Top()
 
 
-@_stack
-def equal(int a, int b):
-    """
-    Returns ``True`` if the two values in acceptable indices ``a`` and ``b`` are equal,
-    following the semantics of the Lua ``==`` operator (that is, may call metamethods).
-    Otherwise returns ``False``. Also returns ``False`` if any of the indices is non valid.
-    """
-    return <bool> lua.Equal(a, b)
+    @_stack
+    def equal(int a, int b):
+        """
+        Returns ``True`` if the two values in acceptable indices ``a`` and ``b`` are equal,
+        following the semantics of the Lua ``==`` operator (that is, may call metamethods).
+        Otherwise returns ``False``. Also returns ``False`` if any of the indices is non valid.
+        """
+        return <bool> lua.Equal(a, b)
 
 
-@_stack
-def raw_equal(int a, int b):
-    """
-    Returns ``True`` if the two values in acceptable indices
-    ``a`` and ``b`` are primitively equal (that is, without calling metamethods).
-    Otherwise returns ``False``. Also returns ``False`` if any of the indices are non valid.
-    """
-    return <bool> lua.RawEqual(a, b)
+    @_stack
+    def raw_equal(int a, int b):
+        """
+        Returns ``True`` if the two values in acceptable indices
+        ``a`` and ``b`` are primitively equal (that is, without calling metamethods).
+        Otherwise returns ``False``. Also returns ``False`` if any of the indices are non valid.
+        """
+        return <bool> lua.RawEqual(a, b)
 
 
-@_stack
-def insert(int destination_index):
-    """Moves the top element into the given valid index, shifting up the elements above this index to open space."""
-    lua.Insert(destination_index)
+    @_stack
+    def insert(int destination_index):
+        """Moves the top element into the given valid index, shifting up the elements above this index to open space."""
+        lua.Insert(destination_index)
 
 
-@_stack
-def throw_error(const char* message):
-    """Raises an error with the given :class:`bytes` message.
+    @_stack
+    def throw_error(const char* message):
+        """Raises an error with the given :class:`bytes` message.
 
-    It also adds at the beginning of the message the file name and the line number where the error occurred,
-    if this information is available.
-    """
-    lua.ThrowError(message)
+        It also adds at the beginning of the message the file name and the line number where the error occurred,
+        if this information is available.
+        """
+        lua.ThrowError(message)
 
 
-@_stack
-def create_table():
-    """Creates a new empty table and pushes it onto the stack."""
-    lua.CreateTable()
+    @_stack
+    def create_table():
+        """Creates a new empty table and pushes it onto the stack."""
+        lua.CreateTable()
 
 
-@_stack
-def push(int stack_pos):
-    lua.Push(stack_pos)
+    @_stack
+    def push(int stack_pos):
+        lua.Push(stack_pos)
 
 
-@_stack
-def push_special(int type):
-    lua.PushSpecial(type)
+    @_stack
+    def push_special(int type):
+        lua.PushSpecial(type)
 
 
-@_stack
-def push_nil():
-    """Pushes a ``nil`` value onto the stack."""
-    lua.PushNil()
+    @_stack
+    def push_nil():
+        """Pushes a ``nil`` value onto the stack."""
+        lua.PushNil()
 
 
-@_stack
-def push_number(double num):
-    """Pushes a number with value ``num`` onto the stack."""
-    lua.PushNumber(num)
+    @_stack
+    def push_number(double num):
+        """Pushes a number with value ``num`` onto the stack."""
+        lua.PushNumber(num)
 
 
-@_stack
-def push_string(const char* val):
-    """Pushes a ``bytes`` onto the stack."""
-    lua.PushString(val)
+    @_stack
+    def push_string(const char* val):
+        """Pushes a ``bytes`` onto the stack."""
+        lua.PushString(val)
 
 
-@_stack
-def push_bool(bool val):
-    """Pushes a boolean value ``val`` onto the stack."""
-    lua.PushBool(val)
+    @_stack
+    def push_bool(bool val):
+        """Pushes a boolean value ``val`` onto the stack."""
+        lua.PushBool(val)
 
 
-@_stack
-def pop(int amt=1):
-    """Pops ``amt`` elements from the stack."""
-    lua.Pop(amt)
+    @_stack
+    def pop(int amt=1):
+        """Pops ``amt`` elements from the stack."""
+        lua.Pop(amt)
 
 
-@_stack
-def clear():
-    """Clears the stack."""
-    pop(top())
+    @_stack
+    def clear():
+        """Clears the stack."""
+        LuaStack.pop(LuaStack.top())
 
 
-@_stack
-def get_table(int stack_pos):
-    """
-    Pushes onto the stack the value ``t[k]``, where ``t`` is the value at the given valid index ``stack_pos``
-    and ``k`` is the value at the top of the stack.
+    @_stack
+    def get_table(int stack_pos):
+        """
+        Pushes onto the stack the value ``t[k]``, where ``t`` is the value at the given valid index ``stack_pos``
+        and ``k`` is the value at the top of the stack.
 
-    This function pops the key from the stack (putting the resulting value in its place).
-    """
-    lua.GetTable(stack_pos)
+        This function pops the key from the stack (putting the resulting value in its place).
+        """
+        lua.GetTable(stack_pos)
 
 
-@_stack
-def get_field(int stack_pos, const char* name):
-    """Pushes onto the stack the value ``t[name]``, where ``t`` is the value at the given valid index ``stack_pos``."""
-    lua.GetField(stack_pos, name)
+    @_stack
+    def get_field(int stack_pos, const char* name):
+        """Pushes onto the stack the value ``t[name]``, where ``t`` is the value at the given valid index ``stack_pos``."""
+        lua.GetField(stack_pos, name)
 
 
-@_stack
-def set_table(int stack_pos):
-    """
-    Does the equivalent to ``t[k] = v``,
-    where ``t`` is the value at the given valid index ``stack_pos``,
-    ``v`` is the value at the top of the stack, and ``k`` is the value just below the top.
+    @_stack
+    def set_table(int stack_pos):
+        """
+        Does the equivalent to ``t[k] = v``,
+        where ``t`` is the value at the given valid index ``stack_pos``,
+        ``v`` is the value at the top of the stack, and ``k`` is the value just below the top.
 
-    This function pops both the key and the value from the stack.
-    """
-    lua.SetTable(stack_pos)
+        This function pops both the key and the value from the stack.
+        """
+        lua.SetTable(stack_pos)
 
 
-@_stack
-def set_field(int stack_pos, const char* name):
-    """
-    Does the equivalent to ``t[k] = v``,
-    where ``t`` is the value at the given valid index ``stack_pos``
-    and ``v`` is the value at the top of the stack.
+    @_stack
+    def set_field(int stack_pos, const char* name):
+        """
+        Does the equivalent to ``t[k] = v``,
+        where ``t`` is the value at the given valid index ``stack_pos``
+        and ``v`` is the value at the top of the stack.
 
-    This function pops the value from the stack.
-    """
-    lua.SetField(stack_pos, name)
+        This function pops the value from the stack.
+        """
+        lua.SetField(stack_pos, name)
 
 
-@_stack
-def call(int args, int results):
-    """Calls a function.
+    @_stack
+    def call(int args, int results):
+        """Calls a function.
 
-    To call a function you must use the following protocol:
+        To call a function you must use the following protocol:
 
-    1. The function to be called is pushed onto the stack
-    2. The arguments to the function are pushed in direct order; that is, the first argument is pushed first.
-    3. You call this function; ``args`` is the number of arguments that you pushed onto the stack.
+        1. The function to be called is pushed onto the stack
+        2. The arguments to the function are pushed in direct order; that is, the first argument is pushed first.
+        3. You call this function; ``args`` is the number of arguments that you pushed onto the stack.
 
-    All arguments and the function value are popped from the stack when the function is called.
-    The function results are pushed onto the stack when the function returns.
-    The number of results is adjusted to ``results``.
-    The function results are pushed onto the stack in direct order (the first result is pushed first),
-    so that after the call the last result is on the top of the stack.
+        All arguments and the function value are popped from the stack when the function is called.
+        The function results are pushed onto the stack when the function returns.
+        The number of results is adjusted to ``results``.
+        The function results are pushed onto the stack in direct order (the first result is pushed first),
+        so that after the call the last result is on the top of the stack.
 
-    The following example shows how the host program can do the equivalent to this Lua code::
+        The following example shows how the host program can do the equivalent to this Lua code::
 
-      a = f("how", t.x, 14)
+          a = f("how", t.x, 14)
 
-    Here it is in GPython::
+        Here it is in GPython::
 
-      push_special(Special.GLOBAL)
-      get_field(1, "f")  # function to be called
-      push_string("how")  # 1st argument
-      get_field(1, "t")  # table to be indexed
-      get_field(-1, "x")  # push result of t.x (2nd arg)
-      remove(-2)  # remove 't' from the stack
-      push_number(14)  # 3rd argument
-      call(3, 1)  # call 'f' with 3 arguments and 1 result
-      set_field(1, "a")  # set global 'a'
+          push_special(Special.GLOBAL)
+          get_field(1, "f")  # function to be called
+          push_string("how")  # 1st argument
+          get_field(1, "t")  # table to be indexed
+          get_field(-1, "x")  # push result of t.x (2nd arg)
+          remove(-2)  # remove 't' from the stack
+          push_number(14)  # 3rd argument
+          call(3, 1)  # call 'f' with 3 arguments and 1 result
+          set_field(1, "a")  # set global 'a'
 
-    Note that the code above is "balanced": at its end, the stack is back to its original configuration.
-    This is considered good programming practice.
-    """
-    lua.Call(args, results)
+        Note that the code above is "balanced": at its end, the stack is back to its original configuration.
+        This is considered good programming practice.
+        """
+        lua.Call(args, results)
 
 
-@_stack
-def get_string(int stack_pos=-1):
-    """Returns the ``bytes`` value of a string item at ``stack_pos`` index of the stack.
+    @_stack
+    def get_string(int stack_pos=-1):
+        """Returns the ``bytes`` value of a string item at ``stack_pos`` index of the stack.
 
-    Negative values can be used for indexing the stack from top.
-    """
-    return lua.GetString(stack_pos, <unsigned int*> 0)
+        Negative values can be used for indexing the stack from top.
+        """
+        return lua.GetString(stack_pos, <unsigned int*> 0)
 
 
-@_stack
-def get_number(int stack_pos=-1):
-    """Returns the ``float`` value of a number item at ``stack_pos`` index of the stack.
+    @_stack
+    def get_number(int stack_pos=-1):
+        """Returns the ``float`` value of a number item at ``stack_pos`` index of the stack.
 
-    Negative values can be used for indexing the stack from top.
-    """
-    return lua.GetNumber(stack_pos)
+        Negative values can be used for indexing the stack from top.
+        """
+        return lua.GetNumber(stack_pos)
 
 
-@_stack
-def get_bool(int stack_pos=-1):
-    """Returns the boolean value of a boolean item at ``stack_pos`` index of the stack.
+    @_stack
+    def get_bool(int stack_pos=-1):
+        """Returns the boolean value of a boolean item at ``stack_pos`` index of the stack.
 
-    Negative values can be used for indexing the stack from top.
-    """
-    return lua.GetBool(stack_pos)
+        Negative values can be used for indexing the stack from top.
+        """
+        return lua.GetBool(stack_pos)
 
 
-@_stack
-def create_ref():
-    """Saves the value at the top of the stack to a reference, pops it and returns the reference."""
-    return lua.ReferenceCreate()
+    @_stack
+    def create_ref():
+        """Saves the value at the top of the stack to a reference, pops it and returns the reference."""
+        return lua.ReferenceCreate()
 
 
-@_stack
-def free_ref(int ref):
-    """Frees the reference."""
-    lua.ReferenceFree(ref)
+    @_stack
+    def free_ref(int ref):
+        """Frees the reference."""
+        lua.ReferenceFree(ref)
 
 
-@_stack
-def push_ref(int ref):
-    """Pushes the value saved in the reference."""
-    lua.ReferencePush(ref)
+    @_stack
+    def push_ref(int ref):
+        """Pushes the value saved in the reference."""
+        lua.ReferencePush(ref)
 
 
-@_stack
-def get_type(int stack_pos):
-    return lua.GetType(stack_pos)
+    @_stack
+    def get_type(int stack_pos):
+        return lua.GetType(stack_pos)
 
 
-@_stack
-def get_type_name(ValueType type):
-    return lua.GetTypeName(type)
+    @_stack
+    def get_type_name(ValueType type):
+        return lua.GetTypeName(type)
 
 
-@_stack
-def is_type(int stack_pos, ValueType type):
-    return lua.IsType(stack_pos, type)
+    @_stack
+    def is_type(int stack_pos, ValueType type):
+        return lua.IsType(stack_pos, type)

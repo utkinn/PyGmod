1cc15cdd59460dfc5508634617fb95de6d33b923
Nikita <shockck84@gmail.com>
2018-10-10 22:24:45 +0500

    Move Player.get_by_user_id staticmethod to module top and rename it to get_by_userid

--- python_extensions/gmod/player.py
+++ python_extensions/gmod/player.py
@@ -8,11 +8,12 @@ from .lua import G
 
 class Player(BaseEntity):
     """Class that wraps the ``Player`` Lua class."""
+    ...
 
-    @staticmethod
-    def get_by_user_id(user_id: int):
-        """Returns the :class:`Player` instance with the user ID ``user_id``.
 
-        *Lua similar:* `Player() <http://wiki.garrysmod.com/page/Global/Player>`_
-        """
-        return Player(G['Player'](user_id))
+def get_by_userid(userid: int):
+    """Returns the :class:`Player` instance with the user ID ``userid``.
+
+    *Lua similar:* `Player() <http://wiki.garrysmod.com/page/Global/Player>`_
+    """
+    return Player(G['Player'](userid))

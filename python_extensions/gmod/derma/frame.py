94d98fcf1c6d2e9bb64fa4567c2cf5beda545dc4
Nikita <shockck84@gmail.com>
2018-11-01 15:19:29 +0500

    Add draggable, resizable properties to Frame

--- python_extensions/gmod/derma/frame.py
+++ python_extensions/gmod/derma/frame.py
@@ -2,9 +2,12 @@ from .. import draw
 from .panel import Panel
 
 
-# noinspection PyMissingConstructor
 class Frame(Panel):
     _lua_class = 'DFrame'
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.draggable = True
 
     @property
     def title(self):
@@ -21,3 +24,23 @@ class Frame(Panel):
 
     def paint(self):
         draw.rounded_box(0, 0, self.w, self.h, (0, 0, 0, 230), 8)
+
+    @property
+    def draggable(self):
+        return bool(self._lua['GetDraggable'](self._lua))
+
+    @draggable.setter
+    def draggable(self, val):
+        if not isinstance(val, bool):
+            raise TypeError('draggable must be bool')
+        self._lua['SetDraggable'](self._lua, val)
+
+    @property
+    def resizable(self):
+        return bool(self._lua['GetSizable'](self._lua))
+
+    @resizable.setter
+    def resizable(self, val):
+        if not isinstance(val, bool):
+            raise TypeError('resizable must be bool')
+        self._lua['SetSizable'](self._lua, val)

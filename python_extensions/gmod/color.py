962e5e17db67701a2a83a9baada9eb61244d3d26
Nikita <shockck84@gmail.com>
2018-11-10 10:32:20 +0500

    Add support for 3-component colors

--- python_extensions/gmod/color.py
+++ python_extensions/gmod/color.py
@@ -17,7 +17,8 @@ class Color:
 
     *Lua similar:* `Color() <http://wiki.garrysmod.com/page/Global/Color>`_
     """
-    def __init__(self, r, g, b, a):
+
+    def __init__(self, r, g, b, a=255):
         _check_component_type(r, g, b, a)
         self.r = r
         self.g = g
@@ -26,12 +27,22 @@ class Color:
 
 
 def get_lua_color(col):
+    """Converts 3- or 4-ints tuple or Color to r, g, b, a."""
     if isinstance(col, Color):
         comps = (col.r, col.g, col.b, col.a)
         _check_component_type(*comps)
         return lua.G['Color'](*comps)
     elif isinstance(col, Iterable):
-        r, g, b, a = col
+        col = tuple(col)
+
+        if len(col) == 3:  # r, g, b
+            r, g, b = col
+            a = 255
+        elif len(col) == 4:  # r, g, b, a
+            r, g, b, a = col
+        else:
+            raise ValueError(f'too much or too few color components: expected 3 or 4, got {len(col)}')
+
         _check_component_type(r, g, b, a)
         return lua.G['Color'](r, g, b, a)
     else:

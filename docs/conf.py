b8a2af2ac71c04313624bd2a27069984fd81e0ed
Nikita <shockck84@gmail.com>
2018-11-14 11:27:48 +0500

    Increase version number

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2018, javabird25 (Protocs)'
 author = 'javabird25 (Protocs)'
 
 # The short X.Y version
-version = '0.9'
+version = '0.10'
 # The full version, including alpha/beta/rc tags
-release = '0.9-alpha'
+release = '0.10-alpha'
 
 
 # -- General configuration ---------------------------------------------------

b2efb35babaf2494ab675cba9aeff217c4b1c405
Nikita <shockck84@gmail.com>
2018-12-27 19:22:21 +0500

    Hide icon after 5 seconds

--- python_extensions/gmod/error_notif.py
+++ python_extensions/gmod/error_notif.py
@@ -1,26 +1,38 @@
 """Script that shows a little icon in the top-left corner when an exception happens."""
 
+import time
+
 from .luanamespace import *
 from .lua import luafunction
 
 __all__ = ['setup', 'show']
 
+# Hide the error icon after n milliseconds
+ICON_HIDE_DELAY = 5
 error_icon, _ = Material('pygmod_error.png')
 should_draw_icon = False
+hide_icon_at = 0
 
 
 def show():
-    global should_draw_icon
+    global should_draw_icon, hide_icon_at
+
     should_draw_icon = True
+    hide_icon_at = time.clock() + ICON_HIDE_DELAY
 
 
 def setup():
     def draw_pygmod_error_icon():
+        global should_draw_icon
+
         if not should_draw_icon:
             return
 
         surface.SetDrawColor(255, 255, 255, 255)
         surface.SetMaterial(error_icon)
         surface.DrawTexturedRect(20, 20, 32, 32)
+
+        if time.clock() > hide_icon_at:
+            should_draw_icon = False
 
     hook.Add('DrawOverlay', 'pygmod_show_error_icon', luafunction(draw_pygmod_error_icon))

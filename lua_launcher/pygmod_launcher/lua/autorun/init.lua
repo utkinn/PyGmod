ea438b0392a741f1530641904712bb044a2ccf7a
Nikita <shockck84@gmail.com>
2019-05-04 13:05:54 +0500

    Lua loader: comments

--- lua_launcher/pygmod_launcher/lua/autorun/init.lua
+++ lua_launcher/pygmod_launcher/lua/autorun/init.lua
@@ -1,4 +1,6 @@
-local success, errorMessage = pcall(require, 'pygmod')
+-- First stage of PyGmod init. This script loads pygmod.dll.
+
+local success, errorMessage = pcall(require, 'pygmod')  -- See bin_modules\client\main.cpp, bin_modules\server\main.cpp
 if not success then
     error('[PyGmod] ERROR: PyGmod binary module is not installed or installed incorrectly. ('..errorMessage..')')
 end

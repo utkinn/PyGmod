6b773e30a2effa06f332e7c1744e4b5d56a45eed
Nikita <shockck84@gmail.com>
2019-05-04 13:05:12 +0500

    Realm binary modules: comments and reformatting

--- bin_modules/client/main.cpp
+++ bin_modules/client/main.cpp
@@ -1,13 +1,18 @@
-#define GMMODULE
+// Second stage of PyGmod init.
+// This binary module runs two functions from pygmod.dll:
+// - pygmod_run() on game startup
+// - pygmod_finalize() on game end
+
+// Macro for exposing GMOD_MODULE_OPEN() and GMOD_MODULE_CLOSE() to Garry's Mod
 #define DLL_IMPORT extern "C" __declspec(dllimport)
 
 #include "Interface.h"
 
 DLL_IMPORT int pygmod_run(lua_State *state, bool client);
-DLL_IMPORT int pygmod_finalize(lua_State* state);
+DLL_IMPORT int pygmod_finalize(lua_State *state);
 
 GMOD_MODULE_OPEN() {
-    return pygmod_run(state, true);
+    return pygmod_run(state, true);  // See bin_modules\main\main.cpp
 }
 
 GMOD_MODULE_CLOSE() {

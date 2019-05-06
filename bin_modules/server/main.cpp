a480956b2d4f13eb92c0be868692598c44885cd7
Steven Burnett <texruska@users.noreply.github.com>
2019-05-06 17:43:18 +0000

    Slight cleanup of bin_modules

--- bin_modules/server/main.cpp
+++ bin_modules/server/main.cpp
@@ -6,7 +6,7 @@
 // Macro for exposing GMOD_MODULE_OPEN() and GMOD_MODULE_CLOSE() to Garry's Mod
 #define DLL_IMPORT extern "C" __declspec(dllimport)
 
-#include "Interface.h"
+#include <GarrysMod/Lua/Interface.h>
 
 DLL_IMPORT int pygmod_run(lua_State *state, bool client);
 DLL_IMPORT int pygmod_finalize(lua_State *state);

238f9aa8b9b738391b810f0c10b70d0da01a223d
Steven Burnett <texruska@users.noreply.github.com>
2019-05-06 09:17:29 +0000

    Add luapyobject back in

---
+++ pygmod/python_extensions/luapyobject.hpp
@@ -0,0 +1,8 @@
+// Lua metatable for representing Python objects.
+
+#pragma once
+#include <GarrysMod/Lua/Interface.h>
+
+using namespace GarrysMod::Lua;
+
+void createLuaPyObjectMetatable(ILuaBase* lua);

f50aba043247a8444673488dfcf5ae537617acf0
javabird25 <shockck84@gmail.com>
2019-05-22 15:50:56 +0500

    Document REPL

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2019, javabird25 (Protocs)'
 author = 'javabird25 (Protocs)'
 
 # The short X.Y version
-version = '0.11'
+version = '0.12'
 # The full version, including alpha/beta/rc tags
-release = '0.11-alpha'
+release = version + '-alpha'
 
 
 # -- General configuration ---------------------------------------------------

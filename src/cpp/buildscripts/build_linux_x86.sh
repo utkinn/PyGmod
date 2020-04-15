47bab561b3adf6e93f9b362628245005b607ee6c
javabird25 <shockck84@gmail.com>
2020-04-15 12:09:46 +0500

    Fix build script after moving to buildscripts/

--- src/cpp/buildscripts/build_linux_x86.sh
+++ src/cpp/buildscripts/build_linux_x86.sh
@@ -3,15 +3,17 @@
 # Abort on errors
 set -e
 
+cd "$(dirname "$0")"
+
 echo 'Building image (this can take a while)...'
-image_id=$(docker build --quiet --file build_linux_x86.dockerfile .)
+image_id=$(docker build --quiet --file build_linux_x86.dockerfile ..)
 echo 'Creating container...'
 container_id=$(docker create $image_id)
 echo 'Copying output archive to host...'
-docker cp $container_id:/build/out.tar .
+docker cp $container_id:/build/out.tar ..
 echo 'Extracting the archive...'
-tar -xf out.tar
+(cd .. && tar -xf out.tar)
 echo 'Cleaning up...'
-rm out.tar
+rm ../out.tar
 docker container rm -f $container_id >/dev/null
 docker image rm -f $image_id >/dev/null

3c45ffe202f5f3e58d635e0f559bb5b88c393cdd
javabird25 <shockck84@gmail.com>
2021-01-17 13:44:09 +0500

    Installer building on Linux

--- src/buildscripts/generate-installer.py
+++ src/buildscripts/generate-installer.py
@@ -13,16 +13,20 @@ def files(*dir, suffix=''):
     return filter(lambda e: e.is_file(), Path(path.join(*dir)).glob('**/*' + suffix))
 
 
-def generate_common(build_path):
+def generate_common(build_path, args):
     with ZipFile(build_path / 'common.zip', 'w') as common_zip:
         for file in files('lua'):
-            common_zip.write(file, Path('garrysmod') / Path(file).relative_to('lua'))
+            common_zip.write(file, Path('garrysmod') / file.relative_to('lua'))
         for file in files('html'):
             common_zip.write(file, Path('garrysmod', 'pygmod') / file)
         for file in files('python', 'pygmod'):
-            common_zip.write(file, Path('garrysmod', 'pygmod') / Path(file).relative_to('python'))
-        for file in files('cpp', 'stdlib', 'lib', suffix='.py'):
-            common_zip.write(file, Path('garrysmod', 'pygmod', 'stdlib') / Path(file).relative_to('cpp', 'stdlib', 'lib'))
+            common_zip.write(file, Path('garrysmod', 'pygmod') / file.relative_to('python'))
+        if sys.platform.startswith('win32'):
+            for file in files('cpp', 'stdlib', 'lib', suffix='.py'):
+                common_zip.write(file, Path('garrysmod', 'pygmod', 'stdlib') / file.relative_to('cpp', 'stdlib', 'lib'))
+        elif sys.platform.startswith('linux'):
+            for file in files('cpp', 'cpython-out', 'lib', 'python' + args.python_version, suffix='.py'):
+                common_zip.write(file, Path('garrysmod', 'pygmod', 'stdlib') / file.relative_to('cpp', 'cpython-out', 'lib', 'python' + args.python_version))
 
 
 def generate_win32(build_path, args):
@@ -30,7 +34,7 @@ def generate_win32(build_path, args):
         bin_build_dir = Path('cpp', 'Debug' if args.debug else 'Release')
         win32_zip.write(bin_build_dir / 'pygmod.dll', 'pygmod.dll')
 
-        python_dll = 'python' + args.python_version 
+        python_dll = 'python' + args.python_version.replace('.', '')
         python_dll += '_d.dll' if args.debug else '.dll'
         win32_zip.write(Path('cpp', 'cpython-prefix', 'src', 'cpython', 'PCBuild', 'win32', python_dll), python_dll)
 
@@ -38,11 +42,23 @@ def generate_win32(build_path, args):
             win32_zip.write(realm_dll, Path('garrysmod', 'lua', 'bin') / realm_dll.relative_to(bin_build_dir))
 
         for file in files('cpp', 'stdlib', 'lib', suffix='.pyd'):
-            win32_zip.write(file, Path('garrysmod', 'pygmod') / Path(file).relative_to('cpp'))
+            win32_zip.write(file, Path('garrysmod', 'pygmod') / file.relative_to('cpp'))
 
 
 def generate_linux(build_path, args):
-    ...
+    with ZipFile(build_path / 'linux32.zip', 'w') as linux32_zip:
+        build_path = Path('cpp')
+        libpython_filename = 'libpython' + args.python_version + '.so.1.0'
+        linux32_zip.write(build_path / Path('cpython-out', 'lib', libpython_filename), Path('bin') / libpython_filename)
+
+        linux32_zip.write(build_path / Path('libpygmod.so'), Path('bin', 'libpygmod.so'))
+
+        for realm_dll in build_path.glob('gm*_pygmod_linux.dll'):
+            linux32_zip.write(realm_dll, Path('garrysmod', 'lua', 'bin') / realm_dll.relative_to(build_path))
+
+        stdlib_path = build_path / Path('cpython-out', 'lib', 'python' + args.python_version, 'lib-dynload')
+        for file in files(stdlib_path):
+            linux32_zip.write(file, Path('garrysmod', 'pygmod', 'stdlib', 'lib-dynload') / file.relative_to(stdlib_path))
 
 
 def generate_pyz(build_path, args):
@@ -58,7 +74,7 @@ def generate_pyz(build_path, args):
 def main():
     argp = ArgumentParser(description='Builds the PyGmod installer.')
     argp.add_argument('-d', '--debug', action='store_true', help='look for files produced by debug build')
-    argp.add_argument('python_version', help='embedded Python version without a dot (i. e. 38 for Python 3.8)')
+    argp.add_argument('python_version', help='embedded Python version (i. e. 3.8)')
     argp.add_argument('out', help='installer path')
     args = argp.parse_args()
 
@@ -66,7 +82,7 @@ def main():
     # for respective archives go
     build_path = Path('installer-build')
     build_path.mkdir(exist_ok=True)
-    generate_common(build_path)
+    generate_common(build_path, args)
     if sys.platform.startswith('win32'):
         generate_win32(build_path, args)
     elif sys.platform.startswith('linux'):

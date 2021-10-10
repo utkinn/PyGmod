dc68fe28c4813909835cad6251f9000c1c222d05
javabird25 <shockck84@gmail.com>
2021-10-10 10:33:07 +0500

    Remove some old sources

--- src/cpp/main/interpreter_states.hpp
+++
@@ -1,7 +0,0 @@
-// This header allows to share *clientInterp and *serverInterp between main.cpp and realms.cpp.
-
-#pragma once
-
-#include <Python.h>
-
-extern PyThreadState *clientInterp, *serverInterp;
--- src/cpp/main/stack_dump.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <GarrysMod/Lua/Interface.h>
-
-// Dumps the contents of the Lua stack to Garry's Mod's console for debugging purposes.
-void stackDump(ILuaBase *);
